28d8dfaa98c415057c87d46169847a5336dc62a0
ljlevy <admin.mathweb@hotmail.fr>
2017-09-12 00:56:02 +0200

    Add files via upload

--- TestPortal/lib/configLog.js
+++ TestPortal/lib/configLog.js
@@ -1,1 +1,14 @@
+    (function () {
+        window.configLog = {
+            getLogger: function (moduleName) {
+                var log = log4javascript.getLogger(moduleName);
 
+                var appender = new log4javascript.BrowserConsoleAppender();
+                appender.setThreshold(log4javascript.Level.DEBUG);
+                var layout = new log4javascript.PatternLayout("%p %c %m%n");
+                appender.setLayout(layout);
+                log.addAppender(appender);
+                return log;
+            }
+        };
+    })();
